bfda5ef072f433c3164698777b8e51697f4c0542
SaqJun <bdhdvzg@gmail.com>
2025-11-21 13:26:01 +0530

    Updating for render

--- server.js
+++ server.js
@@ -59,7 +59,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "https://evolve-website-frontend.vercel.app",
+      "http://localhost:5173"
+    ],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
